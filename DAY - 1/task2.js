b8bfbf655bf4391fb845e88276db08a87ba116a1
CharvyJain <69421337+CharvyJain@users.noreply.github.com>
2020-11-27 21:27:54 +0530

    Create task2.js

---
+++ DAY - 1/task2.js
@@ -0,0 +1,24 @@
+/*
+Write a JavaScript program to check two given numbers and return true if one of the number is 50 or if their 
+sum is 50.
+*/
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript program to check two given numbers and return true if one of the number is 50 or if their sum is 50.</title>
+</head>
+<body>
+  <script>
+  function test50(x, y) 
+  {
+  return ((x == 50 || y == 50) || (x + y == 50));
+  }
+  console.log(test50(50, 50))
+  console.log(test50(20, 50))
+  console.log(test50(20, 20))
+  console.log(test50(20, 30))
+  </script>
+</body>
+</html>
