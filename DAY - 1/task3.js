c42df86bddd9651b13d08dbfad441aeb421d068d
CharvyJain <69421337+CharvyJain@users.noreply.github.com>
2020-11-27 21:29:46 +0530

    Create task3.js

---
+++ DAY - 1/task3.js
@@ -0,0 +1,17 @@
+/*
+Write a javascript program to check the length of the string.
+*/
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Write a javascript program to check the length of the string.</title>
+</head>
+<body>
+  <script>
+  var str = "Java Script";
+  console.log('Given String Length: '+str.length);
+  </script>
+</body>
+</html>
