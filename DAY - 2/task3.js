4ca2f549bc978189802c6a8d518ec931fccc9dbf
CharvyJain <69421337+CharvyJain@users.noreply.github.com>
2020-11-28 21:50:00 +0530

    Create task3.js

---
+++ DAY - 2/task3.js
@@ -0,0 +1,20 @@
+/* 
+Write a JavaScript function which display type and version of browser using alert.
+*/
+
+<html>
+  <head>
+  <meta charset=utf-8 />
+  <title>Write a JavaScript program to calculate days left until next Christmas</title>
+  </head>
+  <body>
+    <script>
+    function getBrowserDetails() {
+      alert(navigator.appName);     
+      alert(navigator.appVersion);
+    }
+
+    getBrowserDetails();
+    </script>
+</body>
+  </html>
