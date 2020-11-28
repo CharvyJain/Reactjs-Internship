600df56768e57602c9f2c2eb5a4bd57c644a6431
CharvyJain <69421337+CharvyJain@users.noreply.github.com>
2020-11-28 20:56:59 +0530

    Create task1.js

---
+++ DAY - 2/task1.js
@@ -0,0 +1,18 @@
+/* 
+Write a javascript function with arguments that squares number.
+*/
+
+<html>
+  <head>
+  <meta charset=utf-8 />
+  <title>Write a javascript function with arguments that squares number.</title>
+  </head>
+  <body>
+    <script>
+    function square(num) {
+      return (num * num);
+    }
+    console.log(square(111));
+    </script>
+</body>
+  </html>
