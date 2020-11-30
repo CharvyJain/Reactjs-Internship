8cbd2cfc11905cf59c4a5656a6e0aefcc56969ae
CharvyJain <69421337+CharvyJain@users.noreply.github.com>
2020-11-30 21:06:11 +0530

    Create task1.js

---
+++ DAY - 4/task1.js
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <title>Finding Sign</title>
+        <script>
+            function getsign(a,b,c) {
+                var str = a + b + c;
+                if( str.includes('+') && str.includes('-')) {
+                    alert("Both plus(+) & minus(-) sign!")
+                } else if(str.includes('+')) {
+                    alert("Plus(+) sign!")
+                } else if(str.includes('-')) {
+                    alert("Minus(-) sign!")
+                } else {
+                    alert("Welcome!")
+                }
+            }
+            function getnumbers() {
+                const num1 = document.getElementById("x").value;
+                const num2 = document.getElementById("y").value;
+                const num3 = document.getElementById("z").value;
+                getsign(num1,num2,num3)
+            }
+        </script>
+    </head>
+    <body>
+        <p>Value 1: </p><input type="number" id="x"><br>
+        <p>Value 2: </p><input type="number" id="y"><br>
+        <p>Value 3: </p><input type="number" id="z"><br>
+        <input type="button" value="OK" onclick="getnumbers()">
+    </body>
+</html>
