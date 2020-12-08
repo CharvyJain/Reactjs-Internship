24b248aa65e848c98988ed64128e6e807699af77
CharvyJain <69421337+CharvyJain@users.noreply.github.com>
2020-12-08 16:05:06 +0530

    Create task1.js

---
+++ DAY - 12/task1.js
@@ -0,0 +1,8 @@
+// write a javascript function to clone an array.
+
+var cloneArray = function(arr) {
+ return arr.slice(0);
+    };
+    
+console.log(cloneArray([96, 36, 53, 89, 0]));
+console.log(cloneArray([59, 12, 30, 2]));
